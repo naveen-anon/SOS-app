2b040eddd26414366a8352b6412dc7536b50a14d
Naveen khatri <naveen1khatri@gmail.com>
2025-12-04 14:46:14 +0530

    Create Home component for Next.js app

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center", padding:"50px"}}>
+      <h1>SOS App</h1>
+      <p>Your Next.js app is working!</p>
+    </div>
+  );
+}
